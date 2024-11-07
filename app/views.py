e06a9516e0e2cc98f76e7a54d627a84cb46da5fc
Biswajit Sethy <114790482+Biswajit6844@users.noreply.github.com>
2024-11-07 08:08:52 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -65,4 +65,5 @@ def index():
        
 # Main function
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to port 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port, debug=True)
